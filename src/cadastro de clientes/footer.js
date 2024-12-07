af4615ee6031b3036709efcf83468295240266fc
GXstav0 <gustavoreisss2005@gmail.com>
2024-12-07 12:19:09 -0300

    atualizando cadastro de clientes

---
+++ src/cadastro de clientes/footer.js
@@ -0,0 +1,7 @@
+document.getElementById('footer').innerHTML = /* html */`
+<a href="https://www.youtube.com/" target="_blank"><img src="assets/images/youtube.png" alt="YouTube"
+        height="25px" width="25px"></a>
+<a href="https://www.instagram.com/" target="_blank"><img src="assets/images/instagram.png" alt="Instagram"
+        height="25px" width="25px"></a>
+<span>Copyright 2024 - Ecocel</span>
+`;
---
+++ src/cadastro de clientes/header.js
@@ -0,0 +1,32 @@
+document.getElementById('header').innerHTML = /* html */`
+    <!--Google fonts icons-->
+    <link rel="stylesheet" href="assets\css\dispositivos.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/b024200ba3.js" crossorigin="anonymous"></script>
+
+    <!-- Logo -->
+    <a href="home.html"><img src="assets/images/logo.jpeg" alt="logo" height="50px" width="50px"></a>
+
+    <!-- Menu mobile -->
+    <nav class="nav">
+        <input type="checkbox" id="checkbox" class="checkbox">
+        <label for="checkbox" class=label-menu>
+            <i class="fa-solid fa-bars"></i>
+            <i class="fa-solid fa-circle-xmark"></i>
+        </label>
+
+        <!-- Links do menu-->
+        <ul class="menu">
+            <li><a id="nav-item-home" href="home.html">Home</a></li>
+            <li><a href="dispositivos.html">Dispositivos</a></li>
+            <li><a href="sustentabilidade.html">Sustentabilidade</a></li>
+            <li><a href="contato.html">Contato</a></li>
+            <li><a href="reviews.html">Reviews</a></li>
+            <li><a id="nav-item-login" href="login.html">Login</a></li>
+            <li><a id="nav-item-cadastro" href="cadastro.html">Cadastro</a></li>
+        </ul>
+    </nav>
+    <script src="https://kit.fontawesome.com/b024200ba3.js" crossorigin="anonymous"></script>
+`;
+
+
