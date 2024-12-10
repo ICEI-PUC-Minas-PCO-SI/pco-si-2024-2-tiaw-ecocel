bab8e6bec4637ad7a5e7f7bade537c96ef713e60
unknown <samara1380@gmail.com>
2024-12-10 13:20:30 -0300

    corrigindo link errado

--- src/public/assets/js/headerDispositivos.js
+++ src/public/assets/js/headerDispositivos.js
@@ -48,7 +48,7 @@ document.getElementById('header').innerHTML = /* html */`
         <ul class="menu">
             <li><a id="nav-item-home" href="/index.html">Home</a></li>
             <li><a href="/dispositivos.html">Dispositivos</a></li>
-            <li><a href="cadastroprodutos.html">Cadastro de produto</a></li>
+            <li><a href="/cadastroprodutos.html">Cadastro de produto</a></li>
             <li><a href="/sustentabilidade.html">Sustentabilidade</a></li>
             <li><a href="/contato.html">Contato</a></li>
             <li><a href="/reviews.html">Reviews</a></li>
